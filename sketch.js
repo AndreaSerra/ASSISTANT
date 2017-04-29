72977ab66f60c75b958d473839b465ab5456fb79
Andrea Serra <andreaandrea.serra@outlook.com>
2017-04-29 16:35:25 +0200

    canvas p5js setup done

--- sketch.js
+++ sketch.js
@@ -1,14 +1,22 @@
 function setup() {
   // Create the canvas
-  var canvas = createCanvas(1920, 925);
+  var l = 1920;
+  var h = 925;
+  var canvas = createCanvas(l, h);
   canvas.parent('sketch-holder');
-  background(200);
+
 }
 
 function draw() {
-  background(200);
+  background(233);
   // An ellipse
-  fill(204, 101, 192, 127);
-  stroke(127, 63, 120);
+  fill('rgba(255, 117, 117, 0.5)');
+  strokeWeight(2);
+  stroke(255, 117, 117);
   ellipse(mouseX, mouseY, 200, 200);
+
+  strokeWeight(45);
+  stroke('rgba(255, 117, 117, 0.5)');
+  noFill();
+  rect(1, 1, 1915, 920);
 }
