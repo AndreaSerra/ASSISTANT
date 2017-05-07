e10baad8e95ca0b5b464b25dcf1f7743512fc61a
Andrea Serra <andreaandrea.serra@outlook.com>
2017-05-07 17:01:31 +0200

    .

--- sketch.js
+++ sketch.js
@@ -3,24 +3,34 @@ function setup() {
   var l = 1920;
   var h = 925;
   var canvas = createCanvas(l, h);
-  canvas.parent('sketch-holder');
+  //canvas.parent('sketch-holder');
+  frameRate(90);
+
+  var ellipseX=mouseX;
+  var ellipseY=mouseY;
+  var rangeOpacity=mouseX;
+  var colorOpacityProva = map(rangeOpacity, 1, 1900, 1, 0);
 }
 
 
 function draw() {
-  background(250);
+  background('rgba(255,255,255, 0.3)');
+
+
   // MOUSE ellipse
   var coloreCerchio = color(r, g, bl);
   fill(coloreCerchio);
   strokeWeight(0);
   stroke(255, 117, 117);
-  ellipse(mouseX, mouseY, a, b);
+  ellipse(ellipseX, ellipseY, a, b);
+
 
   // FIXED ellipse
-  fill('rgba(255, 117, 117, 0.5)');
+  fill('rgba(255, 117, 117, '+colorOpacityProva+' )');
   strokeWeight(0);
   stroke(255, 117, 117);
   ellipse(110, 815, 100, 100);
+
 
   // BORDER
   strokeWeight(20);
