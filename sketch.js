a2d9135ffbd775a489db6a2da4617029d26b503c
Andrea Serra <andreaandrea.serra@outlook.com>
2017-05-08 08:58:18 +0200

    -

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function draw() {
   fill('rgba(255, 117, 117, 0.5 )');
   strokeWeight(0);
   stroke(255, 117, 117);
-  ellipse(110, 815, ellipseDiameter, ellipseDiameter);
+  ellipse(110, 815, counter, counter);
 
 
   // BORDER
